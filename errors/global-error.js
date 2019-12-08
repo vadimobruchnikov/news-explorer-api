b03b8153a6eff8f8403633ae36514d6e56a0dd83
Vadim Obruchnikov <vadimobruchnikov@yandex.ru>
2019-12-08 19:16:26 +0300

    fix globalerror

--- errors/global-error.js
+++ errors/global-error.js
@@ -1,9 +1,10 @@
 const { ErrorMessages } = require('../resources/response-messages');
 
-const globalError = (err, req, res) => {
+const globalError = (err, req, res, next) => {
   const { statusCode = 500, message } = err;
-  const errorMessage = statusCode === 500 ? ErrorMessages.SERVER_ERROR : ` !000! ${message}`;
-  return res.status(statusCode).send({ message: errorMessage });
+  const errorMessage = statusCode === 500 ? ErrorMessages.SERVER_ERROR : message;
+  res.status(statusCode).send({ message: errorMessage });
+  next();
 };
 
 module.exports = globalError;
