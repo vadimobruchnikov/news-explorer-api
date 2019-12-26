9c88a753eeadf5b6ca33db5f0ca586caa623eb9d
Vadim Obruchnikov <vadimobruchnikov@yandex.ru>
2019-12-27 00:22:43 +0300

    fix internal server errors module

--- helpers/crash-test.js
+++ helpers/crash-test.js
@@ -1,10 +1,9 @@
 const { DebugMessages } = require('../resources/response-messages');
-const InternalServerError = require('../errors/internal-server-error');
 
 // Отладочный модуль для тестирования падения сервера
 
 module.exports.crashTest = () => {
   setTimeout(() => {
-    throw new InternalServerError(DebugMessages.SERVER_WILL_BE_CRASHED_DEBUG);
+    throw new Error(DebugMessages.SERVER_WILL_BE_CRASHED_DEBUG);
   }, 0);
 };
